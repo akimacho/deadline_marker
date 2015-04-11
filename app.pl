30247b6edb064e9ddb2cf3ab52a3ba303b7601d9
Akimasa Kimura <kimura.akimasa@gmail.com>
2015-04-11 23:33:07 +0900

    app.pl fixed

--- app.pl
+++ app.pl
@@ -1,12 +1,28 @@
 #!/usr/bin/env perl
 use Mojolicious::Lite;
+use Encode qw/encode_utf8 decode_utf8/;
+use FindBin;
+use lib "$FindBin::Bin/lib/";
+use MyApp::DB;
 
 # Documentation browser under "/perldoc"
 plugin 'PODRenderer';
 
 get '/' => sub {
   my $c = shift;
+
+	my $db = MyApp::DB->new({
+		dsn => 'dbi:SQLite:dbname=test.db'
+	});
+	my $itr = $db->search('Deadline', {});
+	
+	$c->stash->{Deadline} = $itr;
   $c->render(template => 'index');
+};
+
+helper myEncode_utf8 => sub {
+	my ($self, $str) = @_;
+	return decode_utf8($str);
 };
 
 app->start;
