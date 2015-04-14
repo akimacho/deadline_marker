0af51c3cd25ec0f5e50d6b4234a1dc936b53dca3
Akimasa Kimura <kimura.akimasa@gmail.com>
2015-04-14 13:17:18 +0900

    app.pl fixed

--- app.pl
+++ app.pl
@@ -10,14 +10,37 @@ plugin 'PODRenderer';
 
 get '/' => sub {
   my $c = shift;
-
 	my $db = MyApp::DB->new({
 		dsn => 'dbi:SQLite:dbname=test.db'
 	});
 	my $itr = $db->search('Deadline', {});
-	
 	$c->stash->{Deadline} = $itr;
   $c->render(template => 'index');
+};
+
+get '/account' => sub {
+	my $c = shift;
+	$c->render(template => 'account');
+};
+
+get '/account/new' => sub {
+	my $c = shift;
+	my $name = $c->req->param('name');
+	my $event = $c->req->param('event');
+	my $deadline = $c->req->param('deadline');
+	my $db = MyApp::DB->new({
+		dsn => 'dbi:SQLite:dbname=test.db'
+	});
+	$db->insert('Deadline', {
+		name => $name,
+		event => $event,
+		deadline => $deadline,
+		reg_date => '2015-4-13'
+	});
+	$c->stash->{name} = $name;
+	$c->stash->{event} = $event;
+	$c->stash->{deadline} = $deadline;
+	$c->render(template => 'new');
 };
 
 helper mydecode_utf8 => sub {
